eb2fbe0589afaddc09e56b06bd840132974e3af3
Jared Hiller <jaredhiller@cornerstoneagency.com>
2013-11-15 12:53:32 -0500

    fixed sidebar issue through reset of sidebar.php

--- wordpress/wp-content/themes/artiste/sidebar.php
+++ wordpress/wp-content/themes/artiste/sidebar.php
@@ -2,15 +2,8 @@
 
 if( is_page_template('template-home.php') || is_singular('portfolio') || is_tax('skill-type') ) {
     // Display the Home/Portfolio Sidebar
-   dynamic_sidebar('Main Sidebar');
-} 
-
-if( is_page_template('template-full-width.php') || is_singular('portfolio') || is_tax('skill-type') ) {
-    // Display the Home/Portfolio Sidebar
-   dynamic_sidebar('Main Sidebar');
-} 
-
-// else {
+    dynamic_sidebar('Portfolio Sidebar');
+} else {
     // Display the Blog/Page Sidebar
-//     dynamic_sidebar('Portfolio Sidebar');
-//}
+    dynamic_sidebar('Main Sidebar');
+}

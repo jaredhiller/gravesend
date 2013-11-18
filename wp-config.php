2c53dad4e810ac209104e3c4e4024bf8dc9755bf
Jared Hiller <jaredhiller@cornerstoneagency.com>
2013-11-18 13:31:52 -0500

    change database settings

--- wp-config.php
+++ wp-config.php
@@ -88,17 +88,3 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
-
-// Heroku Postgres settings
-if (isset($_ENV["DATABASE_URL"])) 
-{
-    $db = parse_url($_ENV["DATABASE_URL"]);
-    define('DB_NAME', trim($db["path"],"/"));
-    define('DB_USER', $db["user"]);
-    define('DB_PASSWORD', $db["pass"]);
-    define('DB_HOST', $db["host"]);
-} 
-else 
-{
-    die("Can't determine database settings from DATABASE_URL\n");
-}
